ad56ddadc54f1bf602778aa711e9dfe6a4499c67
Amanda Hewitt <amandahewitt@Amandas-MBP.home>
2016-12-09 18:03:39 -0500

    changes routes

--- public/app/index.route.js
+++ public/app/index.route.js
@@ -9,7 +9,10 @@
 
   /** @ngInject */
   function routerConfig($stateProvider, $urlRouterProvider, $locationProvider) {
-    $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
     $stateProvider
 
     .state('login', {
