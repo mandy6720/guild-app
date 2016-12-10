ef5854c6604b6accd292fdc8f439ede2fa425d4a
Amanda Hewitt <amandahewitt@Amandas-MBP.home>
2016-12-09 19:45:36 -0500

    Enable cors

--- app.js
+++ app.js
@@ -136,7 +136,7 @@ router.route('/users/:user_name')
 app.use(express.static(path.join(__dirname, 'public')));
 app.get("/", function(req, res){
   if(req.isAuthenticated()) {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, 'public/dashboard/dashboard.html'));
   } else {
     res.sendFile(path.join(__dirname, 'public/login/login.html'));
   }
