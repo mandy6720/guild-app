07b30c86781e8cd8a8058a406e664fc419b226a2
Amanda Hewitt <amandahewitt@Amandas-MBP.home>
2016-12-09 19:36:38 -0500

    Enable cors

--- app.js
+++ app.js
@@ -39,6 +39,13 @@ passport.use(
 
 var app = express();
 
+// enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Configure app for body parser
 //lets us grab data from body of POST
 app.use(bodyParser.urlencoded({extended: true}));
